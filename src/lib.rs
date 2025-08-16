2df68e58ce3a4073b7a540694262aebe38bdf50c
dengpju <dengpju@163.com>
2025-08-16 18:41:31 +0800

    更新

--- src/lib.rs
+++ src/lib.rs
@@ -55,22 +55,14 @@ pub fn exist(tag: String) -> Option<Auth> {
     }
 }
 
-#[macro_export]
-#[allow(unused_macros)]
-macro_rules! unfold {
-    ($scope:expr, $content:expr) => {
-        $scope.configure($content)
-        //$content
-        /*let contents = include_str!("../.././public/scope").to_string();
-
-        println!("contents {}", contents);*/
-
-        // // 将字符串解析为Rust代码片段
-        // let handler_expr = parse_str::<syn::Expr>(contents.as_str()).unwrap();
-        // quote!(#handler_expr;).into();
-    };
-}
-
+///
+/// Examples
+///```text
+/// async fn test(req: actix_web::HttpRequest) -> impl Responder {
+///     return throw!(&req, errcode::VALID_CODE_ERROR);
+/// }
+///```
+///
 #[macro_export]
 #[allow(unused_macros)]
 macro_rules! throw {
@@ -79,24 +71,18 @@ macro_rules! throw {
     };
 }
 
+///
+/// Examples
+///```text
+/// async fn test(req: actix_web::HttpRequest) -> impl Responder {
+///     return throw_tips!(&req, errcode::VALID_CODE_ERROR, "kkk");
+/// }
+///```
+///
 #[macro_export]
 #[allow(unused_macros)]
 macro_rules! throw_tips {
     ($a:expr, $b:expr, $c:expr) => {
         $b.throw_tips($a, $c)
     };
-}
-
-// rust怎么获取文件名、行号、函数名 https://zhuanlan.zhihu.com/p/19832648722
-#[macro_export]
-#[allow(unused_macros)]
-macro_rules! func {
-    () => {{
-        fn f() {}
-        fn type_name_of<T>(_: T) -> &'static str {
-            std::any::type_name::<T>()
-        }
-        let name = type_name_of(f);
-        &name[..name.len() - 3]
-    }};
 }
